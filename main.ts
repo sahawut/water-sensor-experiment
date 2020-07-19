a498458f039d58d7b3a7eeb6a6e059c077b7012a
sahawut <sahawut@gmail.com>
2020-07-19 17:11:25 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
 let p1 = 0
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(p1)
-})
 basic.forever(function () {
     p1 = pins.analogReadPin(AnalogPin.P1)
-    if (250 > p1) {
-        basic.showString("L")
-    } else if (450 > p1) {
-        basic.showString("M")
-    } else {
-        basic.showString("H")
+    basic.showNumber(p1)
+    if (p1 > 400) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
     }
 })
